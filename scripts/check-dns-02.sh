ca73ceb6abce3553a60e3fbb9e95d8b2d1a3a4ef
Aaron Viehl <hello@admon.in.ua>
2024-04-30 23:03:43 +0200

    GLDDNS added

---
+++ scripts/check-dns-02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Step 1: Get the current IP of ns1.glddns.com using 9.9.9.9 as the resolver
+current_ip=$(dig @9.9.9.9 ns2.glddns.com A +short)
+
+# Step 2: Query for ns1.glddns.com A record using the retrieved IP as the DNS server
+queried_ip=$(dig @ns2.glddns.com ns2.glddns.com A +short)
+
+# Step 3 & 4: Compare the IPs and set the exit code accordingly
+if [ "$queried_ip" == "$current_ip" ]; then
+	echo "DNS resolution is working fine for ns1.glddns.com"
+	exit 0
+else
+	echo "DNS resolution outtage for for ns1.glddns.com"
+	exit 80
+fi
